f9925f430f097b8ba8c920968a5dc696688fad95
Christopher Owen <c.ornine@gmail.com>
2021-01-25 14:02:32 +0000

    test: test remove navigation link on Log In page

--- client/techlog/src/__tests__/Loggout.test.js
+++ client/techlog/src/__tests__/Loggout.test.js
@@ -13,16 +13,20 @@ describe('Loggout page', () => {
     render(<MemoryRouter><Authorised/></MemoryRouter>);
   });
   
-  it('Should navigate to loggin page when clicking "Logout"', () => {
+  it('Should navigate to Log In page when clicking "Logout"', () => {
     const logoutButton = screen.getByText(/log ?out/i);
     fireEvent.click(logoutButton)
     expect(screen.getByText(/log ?in/i)).toBeInTheDocument;
-    
   })
 
-  // it('Should remove other navigation links on Log in page', () => {
-    
-  // })
+  it('Should remove other navigation links on Log In page', () => {
+    const searchLink = screen.getByText(/search/i);
+    const newLink = screen.getByText(/new/i);
+    const logoutButton = screen.getByText(/log ?out/i);
+    fireEvent.click(logoutButton)
+    expect(newLink).not.toBeInTheDocument;
+    expect(searchLink).not.toBeInTheDocument;
+  })
 
   // it('Should return to search page when logging in as User or Admin', () => {
     
