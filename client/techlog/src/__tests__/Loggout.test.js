5a12a7e127192405b15eb72a3e28e0b4ae77001e
Christopher Owen <c.ornine@gmail.com>
2021-01-25 13:50:02 +0000

    test: test logout link directs to log in page

--- client/techlog/src/__tests__/Loggout.test.js
+++ client/techlog/src/__tests__/Loggout.test.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { screen, getByLabelText, render, fireEvent, getByRole, waitFor, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import Authorised from '../Components/App/Authorised';
 
 //? how to test across pages navigated?
+//? Router History Provider?
 
 describe('Loggout page', () => {
+
+  beforeEach(() => {
+    render(<MemoryRouter><Authorised/></MemoryRouter>);
+  });
   
-  it('Should navigate to loggin page when clicking "Loggout"', () => {
-    
-  })
-  
-  it('Should remove other navigation links on Log in page', () => {
+  it('Should navigate to loggin page when clicking "Logout"', () => {
+    const logoutButton = screen.getByText('Logout');
+    fireEvent.click(logoutButton)
+    expect(screen.getByText('log in', {exact: false})).toBeInTheDocument;
     
   })
 
-  it('Should return to search page when logging in as User or Admin', () => {
+  // it('Should remove other navigation links on Log in page', () => {
     
-  })
+  // })
+
+  // it('Should return to search page when logging in as User or Admin', () => {
+    
+  // })
 
 })
