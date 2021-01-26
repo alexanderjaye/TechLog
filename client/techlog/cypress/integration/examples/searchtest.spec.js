950586900316bfb8a2ae63f1d6b8fc5b051f577a
Christopher Owen <c.ornine@gmail.com>
2021-01-26 15:33:45 +0000

    test: add cypress test posting new item and checking its display

--- client/techlog/cypress/integration/examples/searchtest.spec.js
+++ client/techlog/cypress/integration/examples/searchtest.spec.js
@@ -1,11 +1,33 @@
 describe('===SEARCH REPORT TEST===', () => {
 
-  it.only('should load reports on start', () => {
+  // stubbed (mock api call) test with fixtures 
+  it('should load reports on start', () => {
     cy.intercept('GET', 'http://localhost:3002/reports', { fixture: 'reports.json' }).as('getAllReports');
     cy.visit('http://localhost:3000/search');
-    cy.wait('@getAllReports');
-
+    cy.wait('@getAllReports'); // wait for get request
     cy.get('.searchitem__container').should('have.length', 2);
-  })
+  });
+
+  // Testing actual db.
+  it('should receive reports from the db', () => {
+    const reportMock = {
+      title: "Post Report",
+      description: "test test test",
+      tags: [
+        "tag1",
+        "tag2"
+      ],
+      steps: [
+        "step1",
+        "step2"
+      ],
+      images: [],
+      reportId: 98765432,
+      _id: "9876"
+    }
+    cy.request('POST', 'http://localhost:3002/reports', reportMock);
+    cy.visit('http://localhost:3000/search');
+    cy.get('.searchitem__container:last-of-type h3').should('contain', reportMock.title);
+  });
 
 });
